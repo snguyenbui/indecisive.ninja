a4670e3b00039e4d9395be4e2b6f5fa3b392eb4c
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-04-02 18:37:53 -0700

    create tables

---
+++ db/schema/01_polls.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS polls CASCADE;
+CREATE TABLE polls (
+  id SERIAL PRIMARY KEY NOT NULL,
+  description TEXT NOT NULL,
+  creator_email VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL
+);
