7800e4ddf64d8d9d85b451fb59d5c50b98f456d8
Devin MacGillivray <devhmac@gmail.com>
2021-04-03 17:44:08 +0000

    getPollInfo is complete

--- db/queries/pollsQueries.js
+++ db/queries/pollsQueries.js
@@ -24,7 +24,6 @@ const db = require('../../lib/db');
 //     AND id = var ;
 
 const getPollInfo = (id) => {
-
   return db.query(`SELECT polls.id AS poll_id, polls.description, creator_email, created_at, choices.id AS choice_id, choices.option, choices.score
   FROM polls
   JOIN choices ON polls.id = choices.poll_id
@@ -33,6 +32,9 @@ const getPollInfo = (id) => {
   `, [id])
     .then(res => {
       return res.rows;
+    })
+    .catch(err => {
+      console.log('error!', err);
     });
 };
 
