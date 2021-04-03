152f511f1157b625a2a0d44a6965189fd9316360
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-04-02 22:45:53 -0700

    create seeds for all the tables and modified default timestamp

--- db/schema/01_polls.sql
+++ db/schema/01_polls.sql
@@ -1,8 +1,7 @@
--- creat table polls
 DROP TABLE IF EXISTS polls CASCADE;
 CREATE TABLE polls (
   id SERIAL PRIMARY KEY NOT NULL,
   description TEXT NOT NULL,
   creator_email VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP NOT NULL
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
---
+++ db/seeds/03_voters.sql
@@ -0,0 +1,2 @@
+INSERT INTO voters (name, poll_id) VALUES ('Devin', 1);
+INSERT INTO voters (name, poll_id) VALUES ('Sunny', 2);
