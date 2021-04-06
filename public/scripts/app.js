72699e22dcafdc1855ff9ed233e4703cf9d46d72
Devin MacGillivray <devhmac@gmail.com>
2021-04-06 18:12:06 +0000

    adds create poll form dropdown and scroll

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
   console.log('The document is ready!');
 
+  $('.create-poll-button').on('click', (e) => {
+    $('.create-poll-glass').slideDown()
+
+    $('html,body').animate({ scrollTop: 500 }, 'slow');
+
+  })
+
   //makes character counter a live count of text chars in textarea
   $('#add_option').on('click', function(e) {
     $('#option_list').append(`<div class="card">
