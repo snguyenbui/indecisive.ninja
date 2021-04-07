b7db21a31a37cf45a02b4a4ad307806532b14809
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-04-07 02:14:17 +0000

    styling partially completed on voting page and result page

--- public/scripts/chartsScript.js
+++ public/scripts/chartsScript.js
@@ -18,28 +18,55 @@ const barData = {
     label: pollData[0].description,
     data: data,
     backgroundColor: [
-      'rgba(255, 99, 132, 0.2)',
-      'rgba(255, 159, 64, 0.2)',
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(54, 162, 235, 0.2)'
-
+      'rgba(255, 0, 40, 0.5)',
+      'rgba(255, 159, 64, 0.5)',
+      'rgba(255, 205, 86, 0.5)',
+      'rgba(75, 192, 192, 0.5)',
+      'rgba(54, 162, 235, 0.5)',
+      'rgba(153, 102, 255, 0.5)',
+      'rgba(201, 203, 207, 0.5)'
     ],
     borderColor: [
       'rgb(255, 99, 132)',
       'rgb(255, 159, 64)',
       'rgb(255, 205, 86)',
-      'rgb(75, 192, 192)'
-
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
     ],
     borderWidth: 1
   }]
 }
 
+
 let myBarChart = new Chart(CHART, {
   type: 'bar',
   data: barData,
   options: {
-    responsive: false
+    responsive: true,
+    scales: {
+      x: {
+        grid: {
+          display: false,
+          color: 'rgba(255, 255, 255, 0.8)',
+        },
+        ticks: {
+          color: 'rgba(255, 255, 255, 0.8)'
+        }
+      },
+      y: {
+        grid: {
+          tickColor: 'rgba(255, 255, 255, 0.8)',
+          color: 'rgba(255, 255, 255, 0.3)'
+        },
+        ticks: {
+          color: 'rgba(255, 255, 255, 0.8)'
+        }
+      }
+    }
   }
 });
+
+console.log(myBarChart)
 
