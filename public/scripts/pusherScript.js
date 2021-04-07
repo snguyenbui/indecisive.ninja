0dfa865e5168d0c1b02df93fea9277be9b921ef1
Devin MacGillivray <devhmac@gmail.com>
2021-04-07 21:58:43 +0000

    fix but where it was adding sentence to voter log

--- public/scripts/pusherScript.js
+++ public/scripts/pusherScript.js
@@ -5,24 +5,24 @@ const pusher = new Pusher('431d49f7809eb69e6319', {
 const channel = pusher.subscribe('my-channel');
 
 channel.bind(`my-event-${window.location.href.split('/')[4]}`, function(data) {
-    const barChartData = [];
-    const barChartLabels = [];
-    for (item of data.poll) {
-      barChartData.push(item.score);
-      barChartLabels.push(item.option);
-    }
-    myBarChart.data.datasets[0].data = barChartData
-    myBarChart.data.labels = barChartLabels;
-    myBarChart.update();
+  const barChartData = [];
+  const barChartLabels = [];
+  for (item of data.poll) {
+    barChartData.push(item.score);
+    barChartLabels.push(item.option);
+  }
+  myBarChart.data.datasets[0].data = barChartData
+  myBarChart.data.labels = barChartLabels;
+  myBarChart.update();
 
 
-    var el = document.createElement("div");
-    $('.logs').append(`<p>${data.name}<\p>`)
-    el.setAttribute("style","position:absolute;top:10%;left:80%;background-color:white;");
-    el.innerHTML = `${data.name}`;
-    setTimeout(function(){
-      el.parentNode.removeChild(el);
-    },3000);
-    document.body.appendChild(el);
+  var el = document.createElement("div");
+  $('.log-list').append(`<li>${data.name}</li>`)
+  el.setAttribute("style", "position:absolute;top:10%;left:80%;background-color:white;");
+  el.innerHTML = `${data.message}`;
+  setTimeout(function() {
+    el.parentNode.removeChild(el);
+  }, 3000);
+  document.body.appendChild(el);
 });
 
