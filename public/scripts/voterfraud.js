ab3e651f7bd9fb73f189e851b6f1aff00567b8fd
Devin MacGillivray <devhmac@gmail.com>
2021-04-08 19:20:39 +0000

    removing console.logs

--- public/scripts/voterfraud.js
+++ public/scripts/voterfraud.js
@@ -2,14 +2,10 @@ $('document').ready(() => {
 
   let id = $('.poll-id-passthrough').text().trim();
 
-  console.log('this is the id', id);
-
   setTimeout(function() {
     console.log('inside set timeout');
     let url = document.URL;
     let front = url.split(":id")[0];
-
-    console.log('this is where we are sending', `${front}/${id}`);
 
     window.location = `${front}${id}`
   }, 2500)
