bd2e158ce1b0a59f16cbe7768bebb89bbcc76813
Devin MacGillivray <devhmac@gmail.com>
2021-04-09 02:04:28 +0000

    makes voter form width dynamic based on option length

--- public/scripts/voteSubmit.js
+++ public/scripts/voteSubmit.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
   $.get('https://www.cloudflare.com/cdn-cgi/trace', function(data) {
     $('#hidden_ip').val(data.split('\n')[2].split('=')[1]);
   });
+
+  let width = 0;
+  for (let option of $('.button')) {
+    if (option.value.length > width) {
+      width = option.value.length;
+    }
+  };
+
+  width *= 9.1;
+  $('#voterForm').width(width)
 });
